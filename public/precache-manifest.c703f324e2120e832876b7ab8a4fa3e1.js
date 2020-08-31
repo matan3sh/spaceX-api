295bb01cfdabb14141548dd8049b7745f5731954
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-08-31 14:50:21 +0300

    Deploy to Heroku

---
+++ public/precache-manifest.c703f324e2120e832876b7ab8a4fa3e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9271ab99e412f61562a048ae9e6f89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c596854dfb19e6293a6",
+    "url": "/static/js/2.8cdd01e8.chunk.js"
+  },
+  {
+    "revision": "83e895f9a437e69d1a80f15ce6e50621",
+    "url": "/static/js/2.8cdd01e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda7f36e05a3d6663b07",
+    "url": "/static/js/main.79a48786.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
